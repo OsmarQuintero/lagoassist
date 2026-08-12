c2d09f01d01e03472494a513d445d21f590d45fc
OsmarQuintero <quinterovaldzosmar.4@gmail.com>
2026-08-11 18:49:23 -0600

    Bug Arreglado

--- .backup-v2/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.clublago.disciplinas;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
-	List<Disciplina> findByActivoTrueOrderByNombre();
-}
